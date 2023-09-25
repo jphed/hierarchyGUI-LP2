77ca515f2e606f17a5c693747b2e7866714d6632
Jorge Parra <13104@ulsachihuahua.edu.mx>
2023-09-25 13:12:44 -0700

    class empleadocmp created

--- ListaDeEmpleados.py
+++ ListaDeEmpleados.py
@@ -1,54 +1,28 @@
 import tkinter as tk
 from tkinter import ttk
 
+class EmpleadoCmp(ttk.Frame):
+    def __init__(self, master, name, photo, major) -> None:
+        super().__init__(master, borderwidth=2, relief="solid")
+        self.name = name
+        ttk.Label(self, text=self.name).grid(column=0, row=0)
+        self.pht = tk.PhotoImage(file=photo) 
+        ttk.Label(self, image=self.pht).grid(column=0,row=1)
+        ttk.Label(self, text=major).grid(column=0, row=2)
+
 class App():
     def __init__(self) -> None:
         self.window = tk.Tk()
-        self.window.title("List of employee")
+        self.window.title("List of employees")
         self.window.geometry("1000x700")
 
-        #Label employees
-        ttk.Label(self.window, text="Employees").grid(column=0,row=0, ipadx=5, ipady=5)
+        EmpleadoCmp(master=self.window, name="Rodolfo", photo="hierarchyGUI-LP2/img/catSmurf.png",
+                     major='ITIT').grid(column=0,row=0)
 
-# ------------------------------------------------------------------------------------------------------------------ #
-        #Nombre1
-        ttk.Label(self.window, text="Pedro").grid(column=0,row=1, ipadx=5, ipady=5)
 
-        #Imagen1
-        catsmurf = tk.PhotoImage(file='hierarchyGUI-LP2/img/catSmurf.png') 
-        ttk.Label(self.window, image=catsmurf).grid(column=0,row=2)
-
-        #Nombre1
-        ttk.Label(self.window, text="Ing. Sistemas").grid(column=0,row=3, ipadx=5, ipady=5)
-
-# ------------------------------------------------------------------------------------------------------------------ #
-
-# ------------------------------------------------------------------------------------------------------------------ #
-        #Nombre2
-        ttk.Label(self.window, text="Manuel").grid(column=1,row=1, ipadx=5, ipady=5)
-
-        #Imagen2
-        catsmurf2 = tk.PhotoImage(file='hierarchyGUI-LP2/img/catSmurf.png') 
-        ttk.Label(self.window, image=catsmurf2).grid(column=1,row=2)
-
-        #Nombre2
-        ttk.Label(self.window, text="Lic. Administracion").grid(column=1,row=3, ipadx=5, ipady=5)
-
-# ------------------------------------------------------------------------------------------------------------------ #
-
-# ------------------------------------------------------------------------------------------------------------------ #
-        #Nombre3
-        ttk.Label(self.window, text="Maria").grid(column=2,row=1, ipadx=5, ipady=5)
-
-        #Imagen3
-        catsmurf3 = tk.PhotoImage(file='hierarchyGUI-LP2/img/catSmurf.png') 
-        ttk.Label(self.window, image=catsmurf3).grid(column=2,row=2)
-
-        #Nombre3
-        ttk.Label(self.window, text="Arquitecto").grid(column=2,row=3, ipadx=5, ipady=5)
-
-# ------------------------------------------------------------------------------------------------------------------ #
 
         self.window.mainloop()
+
+
 if __name__ == '__main__':
     App()
