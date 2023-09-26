a34fb6109e708269b81d9e776b528012a59a242c
Jorge Parra <13104@ulsachihuahua.edu.mx>
2023-09-26 11:14:55 -0700

    gg

--- ListaDeEmpleados.py
+++ ListaDeEmpleados.py
@@ -1,14 +1,24 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import simpledialog
 
 class EmpleadoCmp(ttk.Frame):
     def __init__(self, master, name, photo, major) -> None:
         super().__init__(master, borderwidth=2, relief="solid")
         self.name = name
-        ttk.Label(self, text=self.name).grid(column=0, row=0)
+        self.amongi = tk.StringVar()
+        self.amongi.set(name)
+        ttk.Label(self, textvariable=self.amongi).grid(column=0, row=0)
+
         self.pht = tk.PhotoImage(file=photo) 
         ttk.Label(self, image=self.pht).grid(column=0,row=1)
         ttk.Label(self, text=major).grid(column=0, row=2)
+
+        ttk.Button(self, text="Cambiar", command=self.cambiarNombre).grid(column=0, row=3,padx=5,pady=5)
+
+    def cambiarNombre(self):
+        nuevo = simpledialog.askstring("Entrada","Nuevo nombre:", parent=self.master)
+        self.amongi.set(nuevo)
 
 class App():
     def __init__(self) -> None:
@@ -18,8 +28,8 @@ class App():
 
         EmpleadoCmp(master=self.window, name="Rodolfo", photo="hierarchyGUI-LP2/img/catSmurf.png",
                      major='ITIT').grid(column=0,row=0)
-
-
+        EmpleadoCmp(master=self.window, name="Manuel", photo="hierarchyGUI-LP2/img/amogus.png",
+                     major='Arquitectura').grid(column=2,row=0)
 
         self.window.mainloop()
 
