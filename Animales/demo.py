ae24fcb967cd43054f2cb1c9ae5bc18cfda9ff92
Jorge Parra <13104@ulsachihuahua.edu.mx>
2023-10-04 13:23:57 -0700

    Improved functionality

--- Animales/demo.py
+++ Animales/demo.py
@@ -4,16 +4,20 @@ from PIL import Image, ImageTk
 from tkinter import simpledialog
 
 class Animal(ttk.Frame):
+    animalsList = []  # Lista de objetos Animal
+
     def __init__(self, master, name):
         super().__init__(master)
         self._name = name
         image = Image.open(f'hierarchyGUI-LP2/Animales/img/{self.name}.png')
-        resized_image = image.resize((200, 200))  # Cambiar la resolución a 200x200
+        resized_image = image.resize((200, 200))
         self.picture = ImageTk.PhotoImage(resized_image)
         self.picture_label = ttk.Label(self, image=self.picture)
         self.picture_label.grid(column=0, row=1)
         self.name_label = ttk.Label(self, text=self.name)
         self.name_label.grid(column=0, row=0)
+        
+        Animal.animalsList.append(self)  # Agregar objeto Animal a la lista
 
     @property
     def name(self):
@@ -30,7 +34,7 @@ class Animal(ttk.Frame):
         return f"Animal: {self.name}"
 
 class App():
-    def __init__(self) -> None:
+    def __init__(self):
         self.window = tk.Tk()
         self.window.title('Animals')
         self.window.geometry('1000x700')
@@ -43,23 +47,51 @@ class App():
         animal2 = Animal(self.window, 'smurfcat')
         
         # Store Animal objects in a list
-        self.animalsList = [animal1, animal2]
+        self.animalsList = Animal.animalsList
+
+        # Add animals to Listbox
+        for animal in self.animalsList:
+            self.myListbox.insert(tk.END, animal.name)
 
         self.add_button = ttk.Button(self.window, text="Add Element", command=self.add_element)
         self.add_button.grid(column=0, row=2, padx=15, pady=15)
         
-        for animal in self.animalsList:
-            self.myListbox.insert(tk.END, animal.name)
-        
-        self.myListbox.bind('<<ListboxSelect>>', self.show_details)
-        
-        self.animal_frame = None
+        # Bind ListboxSelect event to update_picture function
+        self.myListbox.bind("<<ListboxSelect>>", self.update_picture)
+
+        # Set initial picture to first animal in list
+        self.current_animal = self.animalsList[0]
+        self.picture_label = ttk.Label(self.window, image=self.current_animal.picture)
+        self.picture_label.grid(column=1, row=0, padx=15, pady=15)
 
     def add_element(self):
         element = simpledialog.askstring("Add Element", "Enter the element to add:")
         if not element:
             element = "default"
+        # Find corresponding image
+        try:
+            image = Image.open(f'hierarchyGUI-LP2/Animales/img/{element}.png')
+        except FileNotFoundError:
+            image = Image.open(f'hierarchyGUI-LP2/Animales/img/default.png')
+        resized_image = image.resize((200, 200))
+        self.current_animal = Animal(self.window, element)
+        self.current_animal.picture = ImageTk.PhotoImage(resized_image)
+        self.picture_label.configure(image=self.current_animal.picture)
         self.myListbox.insert(tk.END, element)
+
+    def update_picture(self, event):
+        # Get selected item from Listbox
+        selection = event.widget.curselection()
+        if selection:
+            index = selection[0]
+            name = event.widget.get(index)
+            # Find corresponding Animal object
+            for animal in self.animalsList:
+                if animal.name == name:
+                    self.current_animal = animal
+                    break
+            # Update picture
+            self.picture_label.configure(image=self.current_animal.picture)
 
     def show_details(self, event):
         selected_item_index = self.myListbox.curselection()
